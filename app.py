5beeeec5195dca6ba323b83ac10eac06cb4b0683
Sahu <abhibhava32@gmail.com>
2023-04-10 13:26:01 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+from newspaper import Article
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        # Extract the URL entered by the user from the request
+        url = request.form['url']
+        
+        # Validate the URL
+        if not url.startswith('http'):
+            error = 'Enter a valid URL'
+            return render_template('index.html', error=error)
+
+        # Fetch the article information using newspaper3k
+        article = Article(url)
+        article.download()
+        article.parse()
+        article.nlp()
+        
+        # Extract the article information
+        image_url = article.top_image
+        article_title = article.title
+        # published_time = article.publish_date
+        summary = article.summary
+        source = article.source_url
+        keywords = article.keywords
+
+        # Render a new HTML template with the fetched article information
+        return render_template('result.html', image_url=image_url, article_title=article_title, summary=summary, source=source, keywords=keywords)
+    
+    # Render the homepage HTML template
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
